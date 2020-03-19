319c814359a01764bc46645333b022ba899ffdc4
filipHartman <filip.hartman23@gmail.com>
2020-03-19 22:06:48 +0100

    Add solution to the brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const stack = [];
+
+    for (let i = 0; i < str.length; i++) {
+        const currentSymbol = str.charAt(i);
+        const config = findConfig(currentSymbol, bracketsConfig);
+        if (config[0] === config[1]) {
+            if (!stack.includes(currentSymbol)) {
+                stack.push(currentSymbol);
+            } else {
+                const last = stack.pop();
+                if (currentSymbol !== last) {
+                    return false;
+                }
+            }
+        } else {
+            const bracketIndex = getConfigBracketIndex(
+                currentSymbol,
+                bracketsConfig,
+            );
+
+            if (bracketIndex === 0) {
+                stack.push(currentSymbol);
+            } else {
+                const last = stack.pop();
+                if (!config.includes(last)) {
+                    return false;
+                }
+            }
+        }
+    }
+    return stack.length === 0;
+};
+const findConfig = (symbol, bracketsConfig) =>
+    bracketsConfig.find(config => config.includes(symbol));
+const getConfigBracketIndex = (symbol, bracketsConfig) =>
+    bracketsConfig.find(config => config.includes(symbol)).indexOf(symbol);
